4b17f2f34fadb4bf4f43a1c8482da763a214bb91
linyc74 <ylin@nycu.edu.tw>
2023-08-18 11:08:49 +0800

    discard CLI

---
+++ cBioIngest.py
@@ -0,0 +1,5 @@
+from cbio_ingest import Main
+
+
+if __name__ == '__main__':
+    Main().main()
--- cbio_ingest/__init__.py
+++ cbio_ingest/__init__.py
@@ -1,41 +1,35 @@
-import os
-from .template import Settings
-from .cbio_ingest import cBioIngest
+import sys
+from PyQt5.QtWidgets import QApplication
 from .view import View
 from .model import Model
 from .controller import Controller
 
 
-import sys
-from PyQt5.QtWidgets import QApplication
+__VERSION__ = '1.1.0-alpha'
 
 
-def gui():
-    app = QApplication(sys.argv)
-    model = Model()
-    view = View(model)
-    controller = Controller(model, view)
-    sys.exit(app.exec_())
+class Main:
 
+    APP_ID = f'NYCU.Dentistry.cBioIngest.{__VERSION__}'
 
-def main(
-        study_info_xlsx: str,
-        clinical_data_xlsx: str,
-        maf_dir: str,
-        tags_json: str,
-        outdir: str):
+    model: Model
+    view: View
+    controller: Controller
 
-    os.makedirs(outdir, exist_ok=True)
+    def main(self):
+        self.config_taskbar_icon()
 
-    settings = Settings(
-        workdir='workdir',
-        outdir=outdir,
-        threads=1,
-        mock=False,
-        debug=False)
+        app = QApplication(sys.argv)
 
-    cBioIngest(settings).main(
-        study_info_xlsx=study_info_xlsx,
-        clinical_data_xlsx=clinical_data_xlsx,
-        maf_dir=maf_dir,
-        tags_json=None if tags_json.lower() == 'none' else tags_json)
+        self.model = Model()
+        self.view = View(self.model)
+        self.controller = Controller(self.model, self.view)
+
+        sys.exit(app.exec_())
+
+    def config_taskbar_icon(self):
+        try:
+            from ctypes import windll  # only exists on Windows
+            windll.shell32.SetCurrentProcessExplicitAppUserModelID(self.APP_ID)
+        except ImportError as e:
+            print(e, flush=True)
