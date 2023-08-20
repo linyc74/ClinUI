9ef3fee342f84c087c85c9b888da161337153dbe
linyc74 <ylin@nycu.edu.tw>
2023-08-20 16:26:32 +0800

    test write study info

--- test/test_cbio_ingest.py
+++ test/test_cbio_ingest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from os.path import exists
-from src.cbio_ingest import cBioIngest
+from src.cbio_ingest import cBioIngest, WriteStudyInfo
 from .setup import TestCase
 
 
@@ -40,3 +40,67 @@ class TestcBioIngest(TestCase):
         ]:
             with self.subTest(file=file):
                 self.assertTrue(exists(f'{self.outdir}/{file}'))
+
+
+class TestWriteStudyInfo(TestCase):
+
+    def setUp(self):
+        self.set_up(py_path=__file__)
+
+    def tearDown(self):
+        self.tear_down()
+
+    def test_no_tags(self):
+        WriteStudyInfo(self.settings).main(
+            study_info_dict={
+                'type_of_cancer': 'hnsc',
+                'cancer_study_identifier': 'hnsc_nycu_2022',
+                'name': 'Head and Neck Squamous Cell Carcinomas (NYCU, 2022)',
+                'description': 'Whole exome sequencing of 11 precancer and OSCC tumor/normal pairs',
+                'groups': 'PUBLIC',
+                'reference_genome': 'hg38',
+            },
+            tags_dict=None,
+        )
+        expected = f'''\
+type_of_cancer: hnsc
+cancer_study_identifier: hnsc_nycu_2022
+name: Head and Neck Squamous Cell Carcinomas (NYCU, 2022)
+description: Whole exome sequencing of 11 precancer and OSCC tumor/normal pairs
+groups: PUBLIC
+reference_genome: hg38
+'''
+        with open(f'{self.outdir}/meta_study.txt') as fh:
+            actual = fh.read()
+
+        self.assertEqual(expected, actual)
+
+        self.assertTrue(not exists(f'{self.outdir}/tags.json'))
+
+    def test_with_tags(self):
+        WriteStudyInfo(self.settings).main(
+            study_info_dict={
+                'type_of_cancer': 'hnsc',
+                'cancer_study_identifier': 'hnsc_nycu_2022',
+                'name': 'Head and Neck Squamous Cell Carcinomas (NYCU, 2022)',
+                'description': 'Whole exome sequencing of 11 precancer and OSCC tumor/normal pairs',
+                'groups': 'PUBLIC',
+                'reference_genome': 'hg38',
+            },
+            tags_dict={'key': 'val'},
+        )
+        expected = f'''\
+type_of_cancer: hnsc
+cancer_study_identifier: hnsc_nycu_2022
+name: Head and Neck Squamous Cell Carcinomas (NYCU, 2022)
+description: Whole exome sequencing of 11 precancer and OSCC tumor/normal pairs
+groups: PUBLIC
+reference_genome: hg38
+tags_file: tags.json
+'''
+        with open(f'{self.outdir}/meta_study.txt') as fh:
+            actual = fh.read()
+
+        self.assertEqual(expected, actual)
+
+        self.assertTrue(exists(f'{self.outdir}/tags.json'))
