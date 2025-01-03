70c94ab1ca1a3c0b563bfc25a9dcc750fd33bb59
linyc74 <yclin.python@gmail.com>
2025-01-03 09:29:36 +0800

    small tweak

--- src/view.py
+++ src/view.py
@@ -127,7 +127,7 @@ class View(QWidget):
         super().__init__()
         self.model = model
 
-        self.setWindowTitle(self.TITLE)
+        self.setWindowTitle(f'{self.TITLE} ({self.model.schema.NAME})')
         self.setWindowIcon(QIcon(self.ICON_FILE))
         self.setWindowIcon(QIcon(f'{dirname(dirname(__file__))}/{self.ICON_FILE}'))
         self.resize(self.WIDTH, self.HEIGHT)
